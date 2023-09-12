471c9b8416a63ecc98bda47f1f91f48a9d94dc4d
Pronob <pronobkarmoker1@gmail.com>
2023-09-12 09:40:32 +0600

    mini practice project

---
+++ MiniCalculator.dart
@@ -0,0 +1,13 @@
+void main() {
+  int a = 10;
+  int b = 20;
+
+  print("sum : ");
+  print(a + b);
+  print("sub : ");
+  print(a - b);
+  print("mul : ");
+  print(a * b);
+  print("div : ");
+  print(a / b);
+}
