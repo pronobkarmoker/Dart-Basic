9b36c643166dde81419223125dbc957134f7fb0a
Pronob <pronobkarmoker1@gmail.com>
2023-09-14 01:40:47 +0600

    variable

---
+++ Variable.dart
@@ -0,0 +1,19 @@
+void main() {
+  int a = 1;
+  double b = 1.2;
+  String s = "shuvo";
+
+  num x = 3;
+  num y = 3.4;
+
+  var k = 9;
+  var i = "shuvo";
+
+  print(a);
+  print(b);
+  print(s);
+  print(x);
+  print(y);
+  print(k);
+  print(i);
+}
