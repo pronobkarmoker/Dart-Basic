32633f68cd17ef39adc52ed709f7ba3e0a3e006d
Pronob <pronobkarmoker1@gmail.com>
2023-09-15 00:07:59 +0600

    const and final

---
+++ const&final.dart
@@ -0,0 +1,19 @@
+void main(){
+  final age = 21;
+  final name = "shuvo";
+
+  const ag  = 22;
+  const nam = "shuvo karmakar"; // to prevent change. dont use name anymore
+  // same name er hote parbe na variable
+
+  final date = DateTime.now();
+
+  //const date = DateTime.now();  // we can not use const
+  
+  print(age);
+  print(name);
+  print(ag);
+  print(nam);
+  print(date);
+
+}
