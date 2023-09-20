9e1b7ed0fcc9dc4e13b3e17d9c839e31df1be038
Pronob <pronobkarmoker1@gmail.com>
2023-09-20 14:50:25 +0600

    type convertion practice

---
+++ typeConvertion.dart
@@ -0,0 +1,11 @@
+void main() {
+  int age = 20;
+  // double age2 = age;  it is not possible .
+  double age2 = age.toDouble();
+
+  double mark = 99.99;
+  int mark2 = mark.toInt();
+
+  print(age2);
+  print(mark2);
+}
